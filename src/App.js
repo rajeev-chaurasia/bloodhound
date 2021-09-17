ff38b1abd1c67dd0f41a36e4f511a4515b910ea0
maverick-rajeev <b18cs017@nitm.ac.in>
2021-09-17 18:31:01 +0530

    Made some minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BackgroundAnimation from './BackgroundAnimation';
+import Button from '@mui/material/Button';
+import Countdown from "react-countdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div style = {{backgroundColor : "#000000"}}>
+         <BackgroundAnimation />
+         <Button variant="contained">Registeration Link(Coming soon)</Button>
+         <p>Are you ready? hint - Welcome</p>
+         <Countdown date={Date.now() + 10000} />
+      </div>
+    );
+  }
 }
 
 export default App;
