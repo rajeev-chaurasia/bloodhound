8887f55a2dd7167cc0f44eff5491ac1ff6f43490
maverick-rajeev <b18cs017@nitm.ac.in>
2021-09-17 19:17:18 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import BackgroundAnimation from './BackgroundAnimation';
-import Button from '@mui/material/Button';
+import React from "react";
+import BackgroundAnimation from "./BackgroundAnimation";
+import Button from "@mui/material/Button";
 import Countdown from "react-countdown";
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     return (
-      <div style = {{backgroundColor : "#000000"}}>
-         <BackgroundAnimation />
-         <Button variant="contained">Registeration Link(Coming soon)</Button>
-         <p>Are you ready? hint - Welcome</p>
-         <Countdown date={Date.now() + 10000} />
+      <div style={{ backgroundColor: "#000000" }}>
+        <div
+          style={{
+            width: "100%",
+            backgroundColor: "#000000",
+          }}
+        >
+          <BackgroundAnimation />
+        </div>
+        <div style={{ position: "absolute", top: 50, left: "20%" }}>
+          <p style={{ color: "white", fontSize: 90 }}>Blood Hound.</p>
+          <p style={{ color: "white", fontSize: 30 }}>Cognitia 2021</p>
+          <Button variant="contained">Registeration Link(Coming soon)</Button>
+          <p>Are you ready? hint - Welcome</p>
+          <Countdown date="2021-10-06T01:02:03" />
+        </div>
       </div>
     );
   }
